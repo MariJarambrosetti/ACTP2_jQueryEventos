ca148aebcae11e7bfc6ebcbfc1bd926210dc8683
mariana <mariana.j.ambrosetti@gmail.com>
2018-05-31 23:58:57 -0400

    toggleClass red corazon

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,4 +2,10 @@ $(document).ready( function (){
 	$('.card').on('click', function(event){
 		$(this).toggleClass('card--open');
 	});
+
+	$('.card__like').on('click', function(event){
+		event.stopPropagation();
+		event.preventDefault();
+		$(this).toggleClass('card__like--red');
+	});
 });
